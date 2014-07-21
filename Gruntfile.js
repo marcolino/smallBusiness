2554b457f20830f27a5f5798c0d0e254ff2873cf
marcolino <marcosolari@gmail.com>
2014-07-21 12:37:33 +0200

    Added fonts to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -178,7 +178,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-          '<%= yeoman.dist %>/styles/fonts/*'
+          '<%= yeoman.dist %>/fonts/*'
         ]
       }
     },
@@ -312,7 +312,6 @@ module.exports = function (grunt) {
             '*.html',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
-            'fonts/*'
           ]
         }, {
           expand: true,
